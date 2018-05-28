03a39b2a38c644873cb07092b306fc263a8effd4
Andrew Onishuk <aonishuk@hortonworks.com>
2018-05-28 11:06:29 +0300

    BUG-103321. Hiveserver2 fails to start on viewFS enabled cluster (aonishuk)
    Change-Id: Ia5382397e078df0a6c92bdde4bb500f704b9b0d3
    Reviewed-on: https://gerrit.eng.hortonworks.com/r/293897
    Reviewed-by: Oliver Szabo <oszabo@hortonworks.com>
    Tested-by: Zuul <releng@hortonworks.com>

--- src/main/resources/stacks/HDP/3.0/services/AMBARI_METRICS/package/scripts/params.py
+++ src/main/resources/stacks/HDP/3.0/services/AMBARI_METRICS/package/scripts/params.py
@@ -396,6 +396,7 @@ clusterHostInfoDict = config["clusterHostInfo"]
 
 hdfs_site = config['configurations']['hdfs-site']
 default_fs = config['configurations']['core-site']['fs.defaultFS']
+dfs_type = default("/commandParams/dfs_type", "")
 
 import functools
 #create partial functions with common arguments for every HdfsResource call
@@ -412,7 +413,8 @@ HdfsResource = functools.partial(
   principal_name = hdfs_principal_name,
   hdfs_site = hdfs_site,
   default_fs = default_fs,
-  immutable_paths = get_not_managed_resources()
+  immutable_paths = get_not_managed_resources(),
+  dfs_type = dfs_type,
  )
 
 
