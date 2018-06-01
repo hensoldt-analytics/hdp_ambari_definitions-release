e85a6125fc70fcae0a881f98f46742daf9c99209
Aravindan Vijayan <avijayan@hortonworks.com>
2018-06-01 09:24:32 -0700

    BUG-103184 : Pre Upgrade check for AMS hadoop sink in HDP 2.6 to 3.0 EU.
    Change-Id: If0315a97aa5a0b893c99911d83f84e017ab52fd2
    Reviewed-on: https://gerrit.eng.hortonworks.com/r/294851
    Reviewed-by: Siddharth Wagle <swagle@hortonworks.com>
    Reviewed-by: Jonathan Hurley <jhurley@hortonworks.com>
    Reviewed-by: Nate Cole <ncole@hortonworks.com>
    Tested-by: Zuul <releng@hortonworks.com>

--- src/main/resources/stacks/HDP/3.0/services/AMBARI_METRICS/package/scripts/params.py
+++ src/main/resources/stacks/HDP/3.0/services/AMBARI_METRICS/package/scripts/params.py
@@ -393,6 +393,7 @@ hdfs_user = config['configurations']['hadoop-env']['hdfs_user']
 hdfs_principal_name = config['configurations']['hadoop-env']['hdfs_principal_name']
 
 clusterHostInfoDict = config["clusterHostInfo"]
+min_hadoop_sink_version = default("/configurations/ams-env/min_ambari_metrics_hadoop_sink_version", "2.7.0.0")
 
 hdfs_site = config['configurations']['hdfs-site']
 default_fs = config['configurations']['core-site']['fs.defaultFS']
