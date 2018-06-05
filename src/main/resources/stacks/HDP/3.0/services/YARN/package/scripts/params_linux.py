410d132e7a0787e0155b47e5478c328ed24a1d77
Billie Rinaldi <billie@apache.org>
2018-06-05 06:47:45 -0700

    BUG-104511. Rename container-executor property docker.privileged-containers.registries to docker.trusted.registries
    Change-Id: Ie061262765f4f5ef3ee6631916fc9de3401d99d8
    Reviewed-on: https://gerrit.eng.hortonworks.com/r/295210
    Reviewed-by: Chandni Singh <csingh@hortonworks.com>
    Tested-by: Zuul <releng@hortonworks.com>

--- src/main/resources/stacks/HDP/3.0/services/YARN/package/scripts/params_linux.py
+++ src/main/resources/stacks/HDP/3.0/services/YARN/package/scripts/params_linux.py
@@ -563,7 +563,7 @@ else:
 docker_allowed_ro_mounts = config['configurations']['container-executor']['docker_allowed_ro-mounts']
 docker_allowed_rw_mounts = config['configurations']['container-executor']['docker_allowed_rw-mounts']
 docker_privileged_containers_enabled = str(config['configurations']['container-executor']['docker_privileged-containers_enabled']).lower()
-docker_privileged_containers_registries = config['configurations']['container-executor']['docker_privileged-containers_registries']
+docker_trusted_registries = config['configurations']['container-executor']['docker_trusted_registries']
 docker_allowed_volume_drivers = config['configurations']['container-executor']['docker_allowed_volume-drivers']
 
 # ATSv2 integration properties started.
