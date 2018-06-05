ce99ea0dacbd168558f9e17d3bd4871291cc439d
Vitaly Brodetskyi <vbrodetskyi@hortonworks.com>
2018-06-06 01:05:22 +0300

    RMP-10712. Upgrade Spark/Zeppelin/Livy from HDP 2.6 to HDP 3.0.(vbrodetskyi)
    Change-Id: Idfd932143550170045e8e50dc7da2ff8a0103e58
    Reviewed-on: https://gerrit.eng.hortonworks.com/r/295273
    Reviewed-by: Robert Levas <rlevas@hortonworks.com>
    Tested-by: Zuul <releng@hortonworks.com>

--- src/main/resources/stacks/HDP/3.0/services/SPARK2/package/scripts/spark_service.py
+++ src/main/resources/stacks/HDP/3.0/services/SPARK2/package/scripts/spark_service.py
@@ -95,13 +95,13 @@ def spark_service(name, upgrade_type=None, action=None):
         params.HdfsResource(None, action="execute")
 
     if name == 'jobhistoryserver':
-      if upgrade_type is None:
-        create_catalog_cmd = format("{hive_schematool_bin}/schematool -dbType {hive_metastore_db_type} "
+
+      create_catalog_cmd = format("{hive_schematool_bin}/schematool -dbType {hive_metastore_db_type} "
                                     "-createCatalog {default_metastore_catalog} "
                                     "-catalogDescription 'Default catalog, for Spark' -ifNotExists "
                                     "-catalogLocation {default_fs}{spark_warehouse_dir}")
 
-        Execute(create_catalog_cmd,
+      Execute(create_catalog_cmd,
                 user = params.hive_user)
 
       historyserver_no_op_test = format(
