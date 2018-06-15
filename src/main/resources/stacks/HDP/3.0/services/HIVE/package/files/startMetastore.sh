d852ad1fb7e0f87c6789ced5c5c17ffd48eee0e3
Ashutosh Chauhan <hashutosh@apache.org>
2018-06-14 22:32:01 -0500

    BUG-104434 : HS2 logs - some lines missing from hiveserver2.log , goes to /tmp/hive/hive.log
    Change-Id: Ied7f41026879fa775f137ace6102ad363b62ff65
    Reviewed-on: https://gerrit.eng.hortonworks.com/r/297335
    Reviewed-by: Miklos Gergely <mgergely@hortonworks.com>
    Tested-by: Zuul <releng@hortonworks.com>

--- src/main/resources/stacks/HDP/3.0/services/HIVE/package/files/startMetastore.sh
+++ src/main/resources/stacks/HDP/3.0/services/HIVE/package/files/startMetastore.sh
@@ -21,5 +21,5 @@
 #
 HIVE_BIN=${HIVE_BIN:-"hive"}
 
-HIVE_CONF_DIR=$4 $HIVE_BIN --service metastore -hiveconf hive.log.file=hivemetastore.log -hiveconf hive.log.dir=$5 > $1 2> $2 &
+HIVE_CONF_DIR=$4 $HIVE_BIN --service metastore > $1 2> $2 &
 echo $!|cat>$3
