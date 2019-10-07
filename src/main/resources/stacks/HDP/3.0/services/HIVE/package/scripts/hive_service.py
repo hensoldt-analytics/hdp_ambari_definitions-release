8b842aba162418d21226aec7d27feeaf0e77c13e
Lisnichenko Dmitro <dlysnichenko@hortonworks.com>
2019-10-07 18:47:21 +0300

    BUG-121829. One of the hive server2 is found to be down post U11/U12->U13 EU/HOU upgrade + additional fix (dlysnichenko)
    Change-Id: I69cc786f7bb4a90fa6ab97571366a2c2837adcc7

--- src/main/resources/stacks/HDP/3.0/services/HIVE/package/scripts/hive_service.py
+++ src/main/resources/stacks/HDP/3.0/services/HIVE/package/scripts/hive_service.py
@@ -105,11 +105,25 @@ def hive_service(name, action='start', upgrade_type=None):
     daemon_kill_cmd = format("{sudo} kill {pid}")
     daemon_hard_kill_cmd = format("{sudo} kill -9 {pid}")
 
+    if name == 'hiveserver2':
+      # wait for HS2 to die after a "delete" event notification sent to HS2 instance
+      Execute(format("! ({process_id_exists_command})"),
+              tries=60,
+              try_sleep=5,
+              ignore_failures = True
+              )
     Execute(daemon_kill_cmd,
       not_if = format("! ({process_id_exists_command})")
     )
 
     wait_time = 5
+    if name == 'hiveserver2':
+      # wait for HS2 to drain connections
+      Execute(format("! ({process_id_exists_command})"),
+              tries=10,
+              try_sleep=3,
+              ignore_failures = True
+              )
     Execute(daemon_hard_kill_cmd,
       not_if = format("! ({process_id_exists_command}) || ( sleep {wait_time} && ! ({process_id_exists_command}) )"),
       ignore_failures = True
